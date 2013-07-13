45486a327def10d9f1c804c5a71a54eddb8b8b16
Ian Dees <ian.dees@gmail.com>
2013-07-13 14:24:20 -0500

    Initial copy from the josm-plugins openstreetbugs directory.
    Removed some OSB-specific stuff.

---
+++ src/org/openstreetmap/josm/plugins/notes/NotesDownloadLoop.java
@@ -0,0 +1,109 @@
+/* Copyright (c) 2013, Ian Dees
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the project nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.openstreetmap.josm.plugins.notes;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.awt.geom.Point2D;
+import java.util.concurrent.TimeUnit;
+
+import org.openstreetmap.josm.Main;
+
+public class NotesDownloadLoop extends Thread {
+
+    private static NotesDownloadLoop instance;
+
+    private long countdown = TimeUnit.SECONDS.toMillis(1);
+
+    private boolean downloadDone = false;
+
+    private final int INTERVAL = 100;
+
+    private NotesPlugin plugin;
+
+    private Point2D lastCenter;
+
+    public NotesDownloadLoop() {
+        setName(tr("Notes download loop"));
+        start();
+    }
+
+    public static synchronized NotesDownloadLoop getInstance() {
+        if(instance == null) {
+            instance = new NotesDownloadLoop();
+        }
+        return instance;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while(true) {
+                countdown -= INTERVAL;
+
+                // if the center of the map has changed, the user has dragged or
+                // zoomed the map
+                if(Main.map != null && Main.map.mapView != null) {
+                    Point2D currentCenter = Main.map.mapView.getCenter();
+                    if(currentCenter != null && !currentCenter.equals(lastCenter)) {
+                        resetCountdown();
+                        lastCenter = currentCenter;
+                    }
+                }
+
+                // auto download if configured
+                if( Main.pref.getBoolean(ConfigKeys.NOTES_AUTO_DOWNLOAD) && !Main.pref.getBoolean(ConfigKeys.NOTES_API_OFFLINE) &&
+                        plugin != null && plugin.getDialog() != null && plugin.getDialog().isDialogShowing() ) {
+                    if(countdown < 0) {
+                        if(!downloadDone) {
+                            if(plugin != null) {
+                                plugin.updateData();
+                                downloadDone = true;
+                            }
+                        } else {
+                            countdown = -1;
+                        }
+                    }
+                }
+
+                Thread.sleep(INTERVAL);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void resetCountdown() {
+        downloadDone = false;
+        countdown = TimeUnit.SECONDS.toMillis(1);
+    }
+
+    public void setPlugin(NotesPlugin plugin) {
+        this.plugin = plugin;
+    }
+}
---
+++ src/org/openstreetmap/josm/plugins/notes/gui/action/NewIssueAction.java
@@ -0,0 +1,112 @@
+/* Copyright (c) 2013, Ian Dees
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the project nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.openstreetmap.josm.plugins.notes.gui.action;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.gui.widgets.HistoryChangedListener;
+import org.openstreetmap.josm.plugins.notes.ConfigKeys;
+import org.openstreetmap.josm.plugins.notes.NotesPlugin;
+import org.openstreetmap.josm.plugins.notes.api.NewAction;
+import org.openstreetmap.josm.plugins.notes.gui.dialogs.TextInputDialog;
+
+public class NewIssueAction extends OsbAction {
+
+    private static final long serialVersionUID = 1L;
+
+    private NotesPlugin plugin;
+
+    private String result;
+
+    private Point p;
+
+    private NewAction newAction = new NewAction();
+
+    public NewIssueAction(NotesPlugin plugin, Point p) {
+        super(tr("New issue"), plugin.getDialog());
+        this.plugin = plugin;
+        this.p = p;
+    }
+
+    @Override
+    protected void doActionPerformed(ActionEvent e) throws IOException, InterruptedException {
+        List<String> history = new LinkedList<String>(Main.pref.getCollection(ConfigKeys.NOTES_NEW_HISTORY, new LinkedList<String>()));
+        HistoryChangedListener l = new HistoryChangedListener() {
+            public void historyChanged(List<String> history) {
+                Main.pref.putCollection(ConfigKeys.NOTES_NEW_HISTORY, history);
+            }
+        };
+
+        result = TextInputDialog.showDialog(
+                Main.map,
+                tr("Create issue"),
+                tr("Describe the problem precisely"),
+                NotesPlugin.loadIcon("icon_error_add22.png"),
+                history, l);
+
+        if(result == null) {
+            canceled = true;
+        }
+    }
+
+    @Override
+    public void execute() throws IOException {
+        if (result.length() > 0) {
+            result = addMesgInfo(result);
+            Node n = newAction.execute(p, result);
+            plugin.getDataSet().addPrimitive(n);
+            if (Main.pref.getBoolean(ConfigKeys.NOTES_API_DISABLED)) {
+                plugin.updateGui();
+            } else {
+                plugin.updateData();
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return tr("Create: " + result);
+    }
+
+    @Override
+    public OsbAction clone() {
+        NewIssueAction action = new NewIssueAction(plugin, p);
+        action.canceled = canceled;
+        action.p = p;
+        action.result = result;
+        return action;
+    }
+}
