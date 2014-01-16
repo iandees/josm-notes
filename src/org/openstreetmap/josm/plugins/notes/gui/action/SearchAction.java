542dbc71f02ebc49b37a358b637e4e76ab2b5998
Toby Murray <toby.murray@gmail.com>
2014-01-16 02:13:07 -0600

    Formatting whitespace

--- src/org/openstreetmap/josm/plugins/notes/gui/action/SearchAction.java
+++ src/org/openstreetmap/josm/plugins/notes/gui/action/SearchAction.java
@@ -19,24 +19,24 @@ import org.openstreetmap.josm.plugins.notes.gui.dialogs.TextInputDialog;
 
 public class SearchAction extends NotesAction {
 
-	private static final long serialVersionUID = 1L;
-	
-	private NotesPlugin plugin;
-	
-	private String searchTerm;
+    private static final long serialVersionUID = 1L;
 
-	public SearchAction(NotesDialog dialog, NotesPlugin plugin) {
-		super(tr("Reopen note"), dialog);
-		this.plugin = plugin;
-	}
+    private NotesPlugin plugin;
 
-	@Override
-	protected void doActionPerformed(ActionEvent e) throws Exception {
-		if(Main.pref.getBoolean(ConfigKeys.NOTES_API_OFFLINE)) {
-			JOptionPane.showMessageDialog(Main.map, "You must be in online mode to use the search feature");
-			canceled = true;
-			return;
-		}
+    private String searchTerm;
+
+    public SearchAction(NotesDialog dialog, NotesPlugin plugin) {
+        super(tr("Reopen note"), dialog);
+        this.plugin = plugin;
+    }
+
+    @Override
+    protected void doActionPerformed(ActionEvent e) throws Exception {
+        if(Main.pref.getBoolean(ConfigKeys.NOTES_API_OFFLINE)) {
+            JOptionPane.showMessageDialog(Main.map, "You must be in online mode to use the search feature");
+            canceled = true;
+            return;
+        }
         List<String> history = new LinkedList<String>(Main.pref.getCollection(ConfigKeys.NOTES_SEARCH_HISTORY, new LinkedList<String>()));
         HistoryChangedListener l = new HistoryChangedListener() {
             public void historyChanged(List<String> history) {
@@ -48,38 +48,37 @@ public class SearchAction extends NotesAction {
                 tr("<html>Search for notes (will go into offline mode)</html>"),
                 NotesPlugin.loadIcon("find_notes.png"),
                 history, l);
-        
+
         if(searchTerm == null) {
             canceled = true;
             return;
         }
         dialog.refreshNoteStatus();
-	}
+    }
 
-	@Override
-	public void execute() throws Exception {
-		List<Note> searchResults = NotesApi.getNotesApi().searchNotes(searchTerm, 1000, 0);
-		System.out.println("search results: " + searchResults.size());
+    @Override
+    public void execute() throws Exception {
+        List<Note> searchResults = NotesApi.getNotesApi().searchNotes(searchTerm, 1000, 0);
+        System.out.println("search results: " + searchResults.size());
         plugin.getDataSet().clear();
         plugin.getDataSet().addAll(searchResults);
-		Main.map.mapView.repaint();
-		dialog.refreshNoteStatus();
-		dialog.setConnectionMode(true);
-		dialog.showQueuePanel();
-		Main.pref.put(ConfigKeys.NOTES_API_OFFLINE, true);
-	}
-	
-	@Override
-	public String toString() {
-		return tr("Search term: " + searchTerm);
-	}
+        Main.map.mapView.repaint();
+        dialog.refreshNoteStatus();
+        dialog.setConnectionMode(true);
+        dialog.showQueuePanel();
+        Main.pref.put(ConfigKeys.NOTES_API_OFFLINE, true);
+    }
 
-	@Override
-	public NotesAction clone() {
+    @Override
+    public String toString() {
+        return tr("Search term: " + searchTerm);
+    }
+
+    @Override
+    public NotesAction clone() {
         SearchAction action = new SearchAction(dialog, plugin);
         action.canceled = canceled;
         action.searchTerm = searchTerm;
         return action;
-	}
-
+    }
 }
