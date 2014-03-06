f9aca33a1f26b22f6164dd1d90c5d5f6d379efb5
Toby Murray <toby.murray@gmail.com>
2014-03-06 00:20:57 -0600

    Initial attempt at making a progress bar
    The search operation can take a while to complete and was locking JOSM up
    during this time. It is better practice to at least throw up a progress bar.
    
    Currently the progress bar is displayed but then doesn't get closed when the
    search is done.

--- src/org/openstreetmap/josm/plugins/notes/gui/action/SearchAction.java
+++ src/org/openstreetmap/josm/plugins/notes/gui/action/SearchAction.java
@@ -3,19 +3,26 @@ package org.openstreetmap.josm.plugins.notes.gui.action;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 import javax.swing.JOptionPane;
 
 import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.gui.PleaseWaitRunnable;
+import org.openstreetmap.josm.gui.progress.PleaseWaitProgressMonitor;
+import org.openstreetmap.josm.gui.progress.ProgressMonitor;
 import org.openstreetmap.josm.gui.widgets.HistoryChangedListener;
+import org.openstreetmap.josm.io.OsmTransferException;
 import org.openstreetmap.josm.plugins.notes.ConfigKeys;
 import org.openstreetmap.josm.plugins.notes.Note;
 import org.openstreetmap.josm.plugins.notes.NotesPlugin;
 import org.openstreetmap.josm.plugins.notes.api.util.NotesCapableOsmApi;
 import org.openstreetmap.josm.plugins.notes.gui.NotesDialog;
 import org.openstreetmap.josm.plugins.notes.gui.dialogs.TextInputDialog;
+import org.xml.sax.SAXException;
 
 public class SearchAction extends NotesAction {
 
@@ -76,14 +83,24 @@ public class SearchAction extends NotesAction {
             daysClosed = -1;
             Main.pref.putInteger(ConfigKeys.NOTES_SEARCH_DAYS_CLOSED, daysClosed);
         }
-        List<Note> searchResults = NotesCapableOsmApi.getNotesApi().searchNotes(searchTerm, noteLimit, daysClosed);
-        System.out.println("search results: " + searchResults.size());
-        plugin.getDataSet().clear();
-        plugin.getDataSet().addAll(searchResults);
-        plugin.updateGui();
-        dialog.setConnectionMode(true);
-        dialog.showQueuePanel();
-        Main.pref.put(ConfigKeys.NOTES_API_OFFLINE, true);
+
+        final NoteSearchTask task = new NoteSearchTask("Searching for notes");
+        task.initParams(searchTerm, noteLimit, daysClosed);
+        Main.worker.submit(task);
+        Main.worker.submit(new Runnable() {
+            @Override
+            public void run() {
+                if (task.isCanceled() || task.isFailed()) return;
+                List<Note> results = task.getFoundNotes();
+                System.out.println("search results: " + results.size());
+                plugin.getDataSet().clear();
+                plugin.getDataSet().addAll(results);
+                plugin.updateGui();
+                dialog.setConnectionMode(true);
+                dialog.showQueuePanel();
+                Main.pref.put(ConfigKeys.NOTES_API_OFFLINE, true);
+            }
+        });
     }
 
     @Override
@@ -97,5 +114,58 @@ public class SearchAction extends NotesAction {
         action.canceled = canceled;
         action.searchTerm = searchTerm;
         return action;
+    }
+
+    private class NoteSearchTask extends PleaseWaitRunnable {
+
+        public NoteSearchTask(String title) {
+            super(title);
+        }
+
+        private String searchTerm;
+        private Integer bugLimit;
+        private Integer closedDays;
+        private List<Note> returnedNotes;
+        Boolean isCancelled = false;
+        Boolean isFailed = false;
+
+        public void initParams(String searchTerm, Integer bugLimit, Integer closedDays) {
+            this.searchTerm = searchTerm;
+            this.bugLimit = bugLimit;
+            this.closedDays = closedDays;
+            returnedNotes = new ArrayList<Note>();
+        }
+
+        @Override
+        protected void cancel() {
+            Main.info("note search cancelled");
+            isCancelled = true;
+        }
+
+        @Override
+        protected void realRun() throws SAXException, IOException,
+        OsmTransferException {
+            ProgressMonitor monitor = new PleaseWaitProgressMonitor();
+            List<Note> noteList = NotesCapableOsmApi.getNotesApi().searchNotes(searchTerm, bugLimit, closedDays, monitor);
+            Main.info("found notes: " + noteList.size());
+            returnedNotes.addAll(noteList);
+        }
+
+        @Override
+        protected void finish() {
+            // TODO Auto-generated method stub
+        }
+
+        public List<Note> getFoundNotes() {
+            return returnedNotes;
+        }
+
+        public Boolean isCanceled() {
+            return isCancelled;
+        }
+
+        public Boolean isFailed() {
+            return isFailed;
+        }
     }
 }
